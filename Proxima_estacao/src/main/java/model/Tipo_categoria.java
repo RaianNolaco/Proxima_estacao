65af1d6257577f9ae2827cfc50e30a2fd2fed9a4
RaianNolaco <81272272+RaianNolaco@users.noreply.github.com>
2021-06-22 20:13:34 -0300

    olá marilene

---
+++ Proxima_estacao/src/main/java/model/Tipo_categoria.java
@@ -0,0 +1,26 @@
+package model;
+
+public class TipoCategoria{
+	
+	private int id_tipo_categoria;
+	private String nome_categoria;
+	
+	
+
+	
+	public int getId_tipo_categoria() {
+		return id_tipo_categoria;
+	}
+	public void setId_tipo_categoria(int id_tipo_categoria) {
+		this.id_tipo_categoria = id_tipo_categoria;
+	}
+	public String getNome_categoria() {
+		return nome_categoria;
+	}
+	public void setNome_categoria(String nome_categoria) {
+		this.nome_categoria = nome_categoria;
+	}
+	
+	
+	
+}
